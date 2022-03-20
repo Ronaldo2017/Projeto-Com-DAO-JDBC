023134104d59978bd96b1b566fa74939fdab6b3a
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-20 10:51:33 -0300

    Implementado busca por id

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,12 +15,12 @@ public class Program {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
+		
 		System.out.println(seller);
 	}
 
